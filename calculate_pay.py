f3cdd95a477af06d787a8ce1b33b75ec2591ce23
WEN_LONG HUANG <whuang148@my.bcit.ca>
2023-02-06 11:19:32 -0800

    Update calculate_pay.py

--- calculate_pay.py
+++ calculate_pay.py
@@ -1,18 +1,15 @@
 def calculate_pay(hours, wage):
     total_wage = 0
     double_wage = 2 * wage
+    over_time_hours = 0
     if hours <= 0 or wage <= 0:
         return 0
-
     if hours > 40:
-        range_of_hours = range(41, hours+1)
-        for i in range_of_hours:
-            wages
-    if hours <= 40:
-        total_wage += hours * wage
-
-    while hours > 40:
-        total_wage += hours * double_wage
+        total_wage += wage * 40
+        over_time_hours = hours - 40
+        total_wage += double_wage * over_time_hours
+    else:
+        total_wage += wage * hours
     return total_wage
 
 
@@ -21,6 +18,8 @@ def calculate_pay(hours, wage):
 
 def main():
     print("calculate_pay(10,10): ", calculate_pay(10, 10))
+    print()
+    print("calculate_pay(40,10): ", calculate_pay(40, 10))
     print()
     print("calculate_pay(50,10): ", calculate_pay(50, 10))
 
