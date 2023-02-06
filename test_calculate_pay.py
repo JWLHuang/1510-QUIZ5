3893dabe849baef4feff33b9ca712ad09b68b226
ERIC DEAU <edeau@my.bcit.ca>
2023-02-06 11:24:17 -0800

    Update unit tests

--- test_calculate_pay.py
+++ test_calculate_pay.py
@@ -2,6 +2,16 @@ from unittest import TestCase
 
 from calculate_pay import calculate_pay
 
+
 class Calculate_Pay(TestCase):
-    def test_calculate_pay(self):
+    def test_positive_value(self):
         self.assertEqual(100, calculate_pay(10, 10))
+
+    def test_overtime(self):
+        self.assertEqual(600, calculate_pay(50, 10))
+
+    def test_zero_value(self):
+        self.assertEqual(0, calculate_pay(0, 0))
+
+    def test_negative_value(self):
+        self.assertEqual(0, calculate_pay(-10, -10))
